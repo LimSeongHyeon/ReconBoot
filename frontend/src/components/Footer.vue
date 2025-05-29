b6362c904d63d45e008b270e9a13a601228cc21f
LimSeongHyeon <shot7351@gmail.com>
2025-05-29 19:30:29 +0900

    dist 배포방식 변경

--- frontend/src/components/Footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="bg-card border-t border-gray-700 py-2">
-    <div class="text-center text-xs text-gray-500">
-      © 2025 ReconBoot by LimSeongHyeon. All rights reserved.
-    </div>
-  </footer>
-</template> 
